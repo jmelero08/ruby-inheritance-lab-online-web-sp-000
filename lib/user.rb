2aff124d10e35aae29a392bb11239541c79c2d29
Jose Melero Martinez <jmelero08@gmail.com>
2020-11-08 20:30:59 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -2,7 +2,7 @@ class User
   
   attr_accessor :first_name, :last_name 
   
-  def initialize (first_name, last_name)
+  def initialize (first_name = nill, last_name = nill)
     @first_name = first_name
     @last_name = last_name
   end 
